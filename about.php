1c7f7b9b2359cbc3ddca9588cde107416a139f0d
akhila132003 <akhilasalian10@gmail.com>
2025-09-19 12:31:11 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,132 @@
+<?php include 'config.php';
+  $admin=new Admin();
+  ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Art Universe</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Amatic+SC:400,700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/ionicons.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.timepicker.css">
+
+    
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body class="goto-here">
+		
+
+
+    <?php include 'header.php'; ?>
+    
+  
+    <div class="hero-wrap hero-bread" style="background-image: url('images/art.jpg');">
+      <div class="container">
+        <div class="row no-gutters slider-text align-items-center justify-content-center">
+          <div class="col-md-9 ftco-animate text-center">
+          	<p class="breadcrumbs"><span class="mr-2" ><a href="index.php" style="font-family: cursive;color:black">Home</a></span> </p>
+            <h1 class="mb-0 bread" style="font-family: cursive;color:black">About us</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <br>
+    <section class="About_us">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6" style="text-align:justify">
+				<h1>About <span>Art universe</span></h1>
+				<p></p><p>Art universe envisioned as an arena for passionately presenting artworks of quality &amp; lasting in value over the years.One of the pioneers in the art business, the gallery has a history of long association with leading artists.The gallery is one of the reputed art galleries of India &amp; has earned a great amount of good-will from the community of art lovers. The guiding principle of the organization has been to bring the viewers closer to the works of great masters of Indian art.The gallery has continued to be a centre of activity for artists, connoisseurs of art, art-lovers and the public.</p>
+<p></p>
+				 
+				<!--<div class="links">
+				  <a href="#" class="link2">COLLECTORS - SELL YOUR ART</a>
+				</div>-->
+			</div>
+			<div class="col-md-6"><img src="images/b.jpg" width="500px" height="500px" class="img-responsive"></div>
+		</div>
+	</div>
+</section>
+
+
+
+   
+    <!-- END nav -->
+
+   
+
+    
+		<div class="row mb-5">
+         
+        
+         
+		
+		<section class="ftco-section testimony-section">
+      <div class="container">
+        <div class="row justify-content-center mb-5 pb-3">
+         
+        </div>
+        <div class="row ftco-animate">
+            
+               
+      
+
+
+
+              
+             
+          </div>
+        </div>
+        
+      </div>
+    </section>
+
+    
+    <?php include 'footer.php';?>
+    
+    
+  
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/bootstrap-datepicker.js"></script>
+  <script src="js/scrollax.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="js/google-map.js"></script>
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
